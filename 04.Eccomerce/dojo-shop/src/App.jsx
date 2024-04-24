2b2029107978cd19c07c7f348d71955c872aac38
Johnny <jonathanbc2012@gmail.com>
2024-04-24 20:28:40 +0200

    frontend, Navigation.jsx, MainCarousel.jsx created

---
+++ 04.Eccomerce/dojo-shop/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './customer/components/navigation/Navigation'
+
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+      <div>home</div>
+    </>
+  )
+}
+
+export default App
