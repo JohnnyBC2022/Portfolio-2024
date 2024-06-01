d72be9223e3f21eaeb8f57e99c979a47081c5422
Johnny <jonathanbc2012@gmail.com>
2024-06-01 19:28:20 +0200

    random-color created

--- 05.25-react-projects/src/App.jsx
+++ 05.25-react-projects/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Accordian from './components/accordian/Index'
+import RandomColor from './components/random-color/Index'
 
 function App() {
     return (
     <>
+    {/* Accordian component */}
     <Accordian/>
+    {/* Random color component */}
+    <RandomColor/>
     </>
     
   )
